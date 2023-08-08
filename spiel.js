9444cb59e9e3ab80df0cd5b70174b500ca36a94f
judiri <138301718+judiri@users.noreply.github.com>
2023-08-08 16:00:52 +0200

    Update spiel.js

--- spiel.js
+++ spiel.js
@@ -10,4 +10,23 @@ const themeToggle = document.getElementById('theme-toggle');
       themeToggle.setAttribute('src', 'moon.png'); // Switch to dark mode
       isDarkMode = true;
     }
+    document.addEventListener("DOMContentLoaded", function() {
+    var startButton = document.getElementById("startButton");
+    var gameCanvas = document.getElementById("gameCanvas");
+    
+    startButton.addEventListener("click", function() {
+        // Leeren Sie die gesamte Seite
+        document.body.innerHTML = "";
+        
+        // Erstellen Sie ein neues Canvas-Element für das Spiel
+        var canvas = document.createElement("canvas");
+        canvas.id = "gameCanvas";
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        document.body.appendChild(canvas);
+
+        // Hier können Sie den Code zum Laden und Starten Ihres Spiels einfügen
+        // Zum Beispiel: initialisieren Sie das Spiel, laden Sie Ressourcen, usw.
+    });
+});
   });
